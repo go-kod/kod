25ace6ddff5fd14a1d662ee86f1188b89c5c9cef
大可 <hnlq.sysu@gmail.com>
2024-02-17 23:10:50 +0800

    fix: test case

--- cmd/kod/internal/generate_test.go
+++ cmd/kod/internal/generate_test.go
@@ -1,23 +1,9 @@
 package internal
 
 import (
-	"os"
-	"os/exec"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestGenerate(t *testing.T) {
 	execute("generate github.com/go-kod/kod/tests/graphcase/...")
-}
-
-func TestGenerateWithWatch(t *testing.T) {
-	execute("generate -w github.com/go-kod/kod/tests/graphcase/... -t 1s")
-	cmd := exec.Command("sh", "-c", "echo test>test.go")
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	assert.Nil(t, cmd.Run())
-	time.Sleep(2 * time.Second)
 }
