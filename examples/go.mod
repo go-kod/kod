f3dbedb031c105119e2021ab55a30f40884253fc
大可 <hnlq.sysu@gmail.com>
2024-02-19 22:18:10 +0800

    chore: add shorturl example

--- examples/go.mod
+++ examples/go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dominikbraun/graph v0.23.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
---
+++ examples/shorturl/short_url_test.go
@@ -0,0 +1,51 @@
+package shorturl
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/go-kod/kod"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShortURL(t *testing.T) {
+	kod.RunTest(t, func(ctx context.Context, impl Component) {
+		result, err := impl.Generate(ctx, &GenerateRequest{
+			URL:      "https://github.com/go-kod/kod",
+			Duration: time.Second,
+		})
+		assert.Nil(t, err)
+		assert.NotEmpty(t, result.Short)
+
+		getRes, err := impl.Get(ctx, &GetRequest{
+			Short: result.Short,
+		})
+		assert.Nil(t, err)
+		assert.Equal(t, "https://github.com/go-kod/kod", getRes.URL)
+	}, kod.WithConfigFile("./kod.toml"))
+}
+
+func TestShortURLInvalid(t *testing.T) {
+	t.Run("Generate", func(t *testing.T) {
+		t.Parallel()
+
+		kod.RunTest(t, func(ctx context.Context, impl Component) {
+			result, err := impl.Generate(ctx, &GenerateRequest{
+				Duration: time.Second,
+			})
+			assert.NotNil(t, err)
+			assert.Nil(t, result)
+		})
+	})
+
+	t.Run("Get", func(t *testing.T) {
+		t.Parallel()
+
+		kod.RunTest(t, func(ctx context.Context, impl Component) {
+			result, err := impl.Get(ctx, &GetRequest{})
+			assert.NotNil(t, err)
+			assert.Nil(t, result)
+		})
+	})
+}
