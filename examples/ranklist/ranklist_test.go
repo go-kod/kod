4b0cac6ddfe9eac76a87d3d760f59c140b381f3d
大可 <hnlq.sysu@gmail.com>
2024-02-20 08:28:08 +0800

    chore: add benchmark

--- examples/ranklist/ranklist_test.go
+++ examples/ranklist/ranklist_test.go
@@ -50,3 +50,39 @@ func TestRankList(t *testing.T) {
 		assert.Equal(t, []string{"member2"}, list)
 	})
 }
+
+func BenchmarkRankList(t *testing.B) {
+	kod.RunTest(t, func(ctx context.Context, impl Component) {
+		err := impl.Add(ctx, &AddRequest{
+			Key:    "key",
+			Member: "member1",
+			Score:  1,
+		})
+		assert.Nil(t, err)
+
+		err = impl.Add(ctx, &AddRequest{
+			Key:    "key",
+			Member: "member3",
+			Score:  3,
+		})
+		assert.Nil(t, err)
+
+		err = impl.Add(ctx, &AddRequest{
+			Key:    "key",
+			Member: "member2",
+			Score:  2,
+		})
+		assert.Nil(t, err)
+
+		t.ResetTimer()
+		for i := 0; i < t.N; i++ {
+			list, err := impl.RankList(ctx, &RankListRequest{
+				Key: "key",
+				Min: "-inf",
+				Max: "+inf",
+			})
+			assert.Nil(t, err)
+			assert.Equal(t, []string{"member3", "member2", "member1"}, list)
+		}
+	})
+}
