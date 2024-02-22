f0af4e64bb0673973d7b3a41470c333f3eed3273
大可 <hnlq.sysu@gmail.com>
2024-02-22 10:21:30 +0800

    chore: add log examples

---
+++ examples/log/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/go-kod/kod"
+)
+
+type app struct {
+	kod.Implements[kod.Main]
+}
+
+func main() {
+	kod.Run(context.Background(), func(ctx context.Context, app *app) error {
+		// default log level is info
+		app.L(ctx).Info("hello world info")
+
+		// set log level to error
+		kod.FromContext(ctx).LevelVar().Set(slog.LevelError)
+
+		// wont print
+		app.L(ctx).Debug("hello world info1")
+		// wont print
+		app.L(ctx).Info("hello world info1")
+		// will print
+		app.L(ctx).Error("hello world error")
+
+		return nil
+	})
+}
