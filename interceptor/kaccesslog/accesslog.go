273ac8bc474c8e88e7a3652309e8dc8411438468
大可 <hnlq.sysu@gmail.com>
2024-04-02 11:47:46 +0800

    feat: accesslog add cost field

--- interceptor/kaccesslog/accesslog.go
+++ interceptor/kaccesslog/accesslog.go
@@ -3,6 +3,7 @@ package kaccesslog
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"github.com/go-kod/kod/interceptor"
 )
@@ -10,12 +11,15 @@ import (
 // Interceptor returns a server interceptor that logs requests and responses.
 func Interceptor() interceptor.Interceptor {
 	return func(ctx context.Context, info interceptor.CallInfo, req, reply []any, invoker interceptor.HandleFunc) error {
+		now := time.Now()
+
 		err := invoker(ctx, info, req, reply)
 
 		attrs := []slog.Attr{
 			slog.Any("req", req),
 			slog.Any("reply", reply),
 			slog.String("method", info.Method),
+			slog.Int64("cost", time.Since(now).Milliseconds()),
 		}
 
 		level := slog.LevelInfo
