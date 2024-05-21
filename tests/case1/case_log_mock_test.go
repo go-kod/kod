789635880440cc0c766e84facfe689c94f6eec2d
大可 <hnlq.sysu@gmail.com>
2024-05-21 14:44:45 +0800

    fix: mock log count

--- tests/case1/case_log_mock_test.go
+++ tests/case1/case_log_mock_test.go
@@ -18,7 +18,7 @@ func TestMockLog(t *testing.T) {
 	kod.RunTest(t, func(ctx context.Context, k Test1Component) {
 		_, err := k.Foo(ctx, &FooReq{Id: 1})
 		require.Equal(t, "test1:B", err.Error())
-		require.Equal(t, 5, observer.Len(), observer.All())
+		require.Equal(t, 6, observer.Len(), observer.All())
 		require.Equal(t, 2, observer.Filter(func(r slog.Record) bool {
 			return r.Level == slog.LevelError
 		}).Len())
@@ -37,7 +37,7 @@ func TestLogLevelVar(t *testing.T) {
 
 		_, err := k.Foo(ctx, &FooReq{Id: 1})
 		require.Equal(t, "test1:B", err.Error())
-		require.Equal(t, 3, observer.Len(), observer.All())
+		require.Equal(t, 6, observer.Len(), observer.All())
 		require.Equal(t, 2, observer.Filter(func(r slog.Record) bool {
 			return r.Level == slog.LevelError
 		}).Len())
