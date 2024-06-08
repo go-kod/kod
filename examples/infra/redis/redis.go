20d22b1b1866d7ce3725077462a12a306551efb5
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2024-06-08 09:57:57 +0200

    fix: broken redis example
    generated redis package conflicted with imported redis package
    
    Fixes #153

--- examples/infra/redis/redis.go
+++ examples/infra/redis/redis.go
@@ -5,14 +5,14 @@ import (
 
 	"github.com/go-kod/kod"
 	"github.com/go-kod/kod/ext/client/kredis"
-	"github.com/redis/go-redis/v9"
+	goredis "github.com/redis/go-redis/v9"
 )
 
 type impl struct {
 	kod.Implements[Component]
 	kod.WithConfig[config]
 
-	cc *redis.Client
+	cc *goredis.Client
 }
 
 type config struct {
@@ -24,6 +24,6 @@ func (ins *impl) Init(ctx context.Context) error {
 	return nil
 }
 
-func (ins *impl) Client() *redis.Client {
+func (ins *impl) Client() *goredis.Client {
 	return ins.cc
 }
