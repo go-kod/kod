e742c0cf42ca334dd6fe0908fb251cc514bb2448
大可 <hnlq.sysu@gmail.com>
2024-06-17 16:07:46 +0800

    chore: add test case

---
+++ cmd/kod/internal/generate_file_test.go
@@ -0,0 +1,40 @@
+// Package files contains file-related utilities.
+package internal
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWriter(t *testing.T) {
+	file := filepath.Join(t.TempDir(), "test")
+	w := NewWriter(file)
+
+	w.Write([]byte("hello"))
+
+	data, err := os.ReadFile(w.tmpName)
+	require.Nil(t, err)
+	require.Equal(t, "hello", string(data))
+
+	w.Close()
+
+	data, err = os.ReadFile(file)
+	require.Nil(t, err)
+	require.Equal(t, "hello", string(data))
+}
+
+func TestWriter1(t *testing.T) {
+	file := filepath.Join(t.TempDir(), "test")
+	w := NewWriter(file)
+
+	w.Write([]byte("hello"))
+
+	data, err := os.ReadFile(w.tmpName)
+	require.Nil(t, err)
+	require.Equal(t, "hello", string(data))
+
+	w.Cleanup()
+}
