94f1288ca2ad1bd82bd13b8b34779853948c9f0d
大可 <hnlq.sysu@gmail.com>
2025-01-13 11:43:44 +0800

    revert

--- tests/case1/case_runtest_test.go
+++ tests/case1/case_runtest_test.go
@@ -11,6 +11,8 @@ import (
 )
 
 func TestTest(t *testing.T) {
+	t.Parallel()
+
 	kod.RunTest(t, func(ctx context.Context, k *test1Component) {
 		_, err := k.Foo(ctx, &FooReq{})
 		fmt.Println(err)
@@ -19,6 +21,8 @@ func TestTest(t *testing.T) {
 }
 
 func TestTest2(t *testing.T) {
+	t.Parallel()
+
 	kod.RunTest2(t, func(ctx context.Context, k *test1Component, k2 Test2Component) {
 		_, err := k.Foo(ctx, &FooReq{})
 		fmt.Println(err)
@@ -27,6 +31,8 @@ func TestTest2(t *testing.T) {
 }
 
 func TestTest3(t *testing.T) {
+	t.Parallel()
+
 	require.Panics(t, func() {
 		kod.RunTest3(t, func(ctx context.Context, k *test1Component, k2 panicNoRecvoeryCaseInterface, k3 test1Controller) {
 			_, err := k.Foo(ctx, &FooReq{})
